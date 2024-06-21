9b2fa2f4d6c9fcc24a88f8c333b4fcf0781d5ece
Esteban <baab2an@live.fr>
2024-06-21 10:44:00 +0200

    FEAT image pizza home pour le header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
+import ImageHome from "../../public/p-home.png";
+
 export default function Header() {
   return (
-    <div className="h-[70vh] max-w-[1200px] m-auto grid grid-cols-1 md:grid-cols-2 p-5">
+    <div className="h-[80vh] max-w-[1200px] m-auto grid grid-cols-1 md:grid-cols-2 p-5 pt-[150px]">
       <div className="order-2 md:order-1 flex justify-center flex-col">
         <h1 className="text-xl md:text-4xl lg:text-6xl text-stone-700 font-black text-center md:text-left mb-2">
           Le monde des Pizzas Gourmandes
@@ -15,6 +18,14 @@ export default function Header() {
         >
           Découvrir nos pizzas
         </a>
+      </div>
+      <div className="order-1 md:order-2 flex items-center justify-center flex-col">
+        <Image
+          src={ImageHome}
+          width={1000}
+          alt="Pizza home"
+          className="w-[400px]"
+        />
       </div>
     </div>
   );
